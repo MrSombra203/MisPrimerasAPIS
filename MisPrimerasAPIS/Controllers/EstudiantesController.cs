3fda28517f32c82c5d174f4e5c20a4695636851b
MrSombra203 <jose.vasconez.andrade@udla.edu.ec>
2024-10-21 08:32:42 -0500

    Se agrego el get de las apis.

---
+++ MisPrimerasAPIS/Controllers/EstudiantesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MisPrimerasAPIS.Modelos;
+
+namespace MisPrimerasAPIS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EstudiantesController : ControllerBase
+    {
+        [Route("InformacionEstudiantes")]
+        [HttpGet]
+        public Estudiante DevueveInformacionEstudiantes()
+        {
+            Estudiante estudiante = new Estudiante
+            {
+                Id = 1,
+                Nombre = "Satiago",
+                Edad = 30,
+                FechaNacimiento = DateTime.Now,
+                EstaInscrito = true,
+            };
+            return estudiante;
+        }
+    }
+}
---
+++ MisPrimerasAPIS/Repository/EstudianteRepository.cs
@@ -0,0 +1,17 @@
+﻿using MisPrimerasAPIS.Modelos;
+
+namespace MisPrimerasAPIS.Repository
+{
+    public class EstudianteRepository
+    {
+        Estudiante estudiante = new Estudiante
+        {
+            Id = 1,
+            Nombre = "Satiago",
+            Edad = 30,
+            FechaNacimiento = DateTime.Now,
+            EstaInscrito = true,
+        };
+        return estudiante;
+    }
+}
