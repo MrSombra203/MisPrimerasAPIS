cdee54cd6a3dd66d7cb42114457b02a62f41de47
laboratorio <laboratorio@UPOAULA10205>
2024-10-21 07:53:48 -0500

    Agregar archivos de proyecto.

---
+++ MisPrimerasAPIS/Modelos/Estudiante.cs
@@ -0,0 +1,12 @@
+﻿namespace MisPrimerasAPIS.Modelos
+{
+    public class Estudiante
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public Boolean EstaInscrito { get; set; }
+        public int Edad {  get; set; }
+
+    }
+}
